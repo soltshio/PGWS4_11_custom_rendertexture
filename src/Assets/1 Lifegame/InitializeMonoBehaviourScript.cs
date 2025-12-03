9ef27407a1463ba41d0bf6328905ffb886b92561
soltshio <shanshanshien@gmail.com>
2025-12-03 14:39:30 +0900

    lifeGameまで完了

---
+++ src/Assets/1 Lifegame/InitializeMonoBehaviourScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class InitializeMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] CustomRenderTexture tex = default!;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tex.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
